109b0e4051d84a43927b4f1775d53838b872511f
Petro Podrezo <petro@podrezo.com>
2020-02-05 22:53:58 -0500

    Move styles out of component, add prop for prompt

--- src/ReactCliComponent.jsx
+++ src/ReactCliComponent.jsx
@@ -2,44 +2,65 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-    label: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    styles: PropTypes.object
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  styles: PropTypes.object
 }
 
 const defaultProps = {
-    styles: {
-        label: {
-            fontFamily: 'Comic Sans MS',
-            color: 'green'
-        },
-        input: {
-            background: '#ddd',
-            border: '1px solid red'
-        }
-    }
+  prompt: '> '
 }
 
 class ReactCliComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      buffer: ['Welcome!']
     }
+    this.promptRef = React.createRef();
+    this._focusPrompt = this._focusPrompt.bind(this);
+    this._handleKeyDown = this._handleKeyDown.bind(this);
+  }
 
-    handleChange(e) {
-        this.props.onChange(e.target.value);
-    }
-    
-    render() {
-        const styles = this.props.styles || {};
+  _focusPrompt() {
+    this.promptRef.current.focus();
+  }
 
-        return (
-            <div>
-                <label style={styles.label}>{this.props.label}</label>
-                <input type="text" style={styles.input} onChange={this.handleChange} />
-            </div>
-        );
+  _handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      const command = this.promptRef.current.innerText;
+      this.setState({
+        buffer: [...this.state.buffer, command]
+      })
+      this.promptRef.current.innerText = '';
+      console.log(`Execute command: ${command}`);
     }
+  }
+
+  // _handlePromptChange(e) {
+  //   console.log(this.promptRef);
+  //   this.setState({
+  //     promptValue: this.promptRef.innerText
+  //   })
+  // }
+  
+  componentDidMount() {
+    //autofocus
+    this._focusPrompt();
+  }
+
+  render() {
+    const styles = this.props.styles || {};
+
+    const lines = this.state.buffer.map((line, index) => <p key={index}>{line}</p>);
+
+    return (
+      <div style={styles.cli} onClick={this._focusPrompt} className="react_cli">
+        {lines}
+        {this.props.prompt} <span spellCheck="true" contentEditable="true" style={{minWidth: '10px'}} onKeyDown={this._handleKeyDown} ref={this.promptRef}></span>
+      </div>
+    );
+  }
 }
 
 ReactCliComponent.propTypes = propTypes;
